b6a84eebb3898897e32c4e77478f03c3bb0143c1
Angela Jaw <ajaw2@illinois.edu>
2018-02-06 14:51:55 -0600

    new changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("here");
+        System.out.println(WebScraper.urlToString ("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(WebScraper.wordFinder("http://erdani.com/tdpl/hamlet.txt", "http://erdani.com/tdpl/hamlet.txt"));
     }
 }
